edbe7615267d5e82488233a4f958c1ce003135a4
Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>
2024-01-02 13:07:29 +0100

    Version 2.5.9

--- pwnagotchi/utils.py
+++ pwnagotchi/utils.py
@@ -261,10 +261,10 @@ def load_config(args):
         config['ui']['display']['type'] = 'waveshare27inch'
 
     elif config['ui']['display']['type'] in ('ws_27inchv2', 'ws27inchv2', 'waveshare_27inchv2', 'waveshare27inchv2'):
-        config['ui']['display']['type'] = 'waveshare27inchv2'
+        config['ui']['display']['type'] = 'waveshare2in7_v2'
 
     elif config['ui']['display']['type'] in ('ws_29inch', 'ws29inch', 'waveshare_29inch', 'waveshare29inch'):
-        config['ui']['display']['type'] = 'waveshare29inch'
+        config['ui']['display']['type'] = 'waveshare2in9'
 
     elif config['ui']['display']['type'] in 'lcdhat':
         config['ui']['display']['type'] = 'lcdhat'
