51d78ef107c941d4951ae36b282ae5a046f4e630
Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>
2024-01-04 12:35:04 +0100

    Version 2.6.1

--- pwnagotchi/ui/web/handler.py
+++ pwnagotchi/ui/web/handler.py
@@ -189,12 +189,9 @@ class Handler:
             return 'success' if plugins.toggle_plugin(request.form['plugin'], checked) else 'failed'
 
         if name == 'upgrade' and request.method == 'POST':
-            try:
-                logging.info(f"Upgrading plugin: {request.form['plugin']}")
-                os.system(f"pwnagotchi plugins update && pwnagotchi plugins upgrade {request.form['plugin']}")
-                return redirect(flask.url_for('/plugins'))
-            except:
-                return 'failed'
+            logging.info(f"Upgrading plugin: {request.form['plugin']}")
+            os.system(f"pwnagotchi plugins update && pwnagotchi plugins upgrade {request.form['plugin']}")
+            return 'success' if redirect(flask.url_for('/plugins')) else 'failed'
 
         if name in plugins.loaded and plugins.loaded[name] is not None and hasattr(plugins.loaded[name], 'on_webhook'):
             try:
